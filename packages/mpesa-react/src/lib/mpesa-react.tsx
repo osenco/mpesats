5ee0cfe17b7902078ca12cab36b49b306d5a1ebc
Odhiambo Dormnic <ayimdomnic@outlook.com>
2021-10-30 16:56:45 +0300

    - Add Nest and React presets and ensure all is bult concurrently

---
+++ packages/mpesa-react/src/lib/mpesa-react.tsx
@@ -0,0 +1,14 @@
+import './mpesa-react.module.css';
+
+/* eslint-disable-next-line */
+export interface MpesaReactProps {}
+
+export function MpesaReact(props: MpesaReactProps) {
+  return (
+    <div>
+      <h1>Welcome to MpesaReact!</h1>
+    </div>
+  );
+}
+
+export default MpesaReact;
